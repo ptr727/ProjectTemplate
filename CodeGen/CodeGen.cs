aff12c1fbe42352da5ced1785002cf78e4273047
Pieter Viljoen <ptr727@users.noreply.github.com>
2026-04-16 12:40:11 -0700

    Update GitHub workflows and API key handling (#54)
    Enhance GitHub workflows by requiring an API key and simplifying secret
    inheritance. Fix formatting for the API key reference in the codegen
    workflow and update the README to include instructions for additional
    secrets. Modify launch configurations and update the QuoteOfTheDay in
    CodeGen.
    
    ---------
    
    Signed-off-by: Pieter Viljoen <ptr727@users.noreply.github.com>

--- CodeGen/CodeGen.cs
+++ CodeGen/CodeGen.cs
@@ -3,11 +3,12 @@ namespace ptr727.ProjectTemplate.CodeGen;
 [System.CodeDom.Compiler.GeneratedCode("ptr727.ProjectTemplate.CodeGen", "1.0")]
 internal static class CodeGen
 {
-    private const string QuoteOfTheDay = "No API key provided.";
+    private const string QuoteOfTheDay =
+        "Don't accept a life that has been molded for you by others because eventually you'll succumb to its falseness.";
 
     internal static void Quote()
     {
-        const string dateTime = "2026-04-13T22:37:43.5686751Z";
+        const string dateTime = "2026-04-16T19:26:19.4402570Z";
         Console.WriteLine($"{dateTime} : {QuoteOfTheDay}");
         Log.Logger.Information("Quote of the Day: {DateTime} : {Quote}", dateTime, QuoteOfTheDay);
     }
